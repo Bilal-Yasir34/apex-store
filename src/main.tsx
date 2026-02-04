e9dbc33be5502ee211aae19bfb760296cf02a410
Bilal-Yasir34 <bilalyasir34@gmail.com>
2026-02-05 04:54:48 +0500

    Switch to HashRouter to fix 404 errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+// Change BrowserRouter to HashRouter
+import { HashRouter as Router } from 'react-router-dom';
 import App from './app';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
