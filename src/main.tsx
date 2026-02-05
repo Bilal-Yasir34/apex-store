230de99c37d82d3ea547666f74dd07966ff65bef
Bilal-Yasir34 <bilalyasir34@gmail.com>
2026-02-05 05:05:11 +0500

    Force clean routing config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Change BrowserRouter to HashRouter
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
