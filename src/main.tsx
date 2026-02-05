08a9ad33468c19af119f276f3ee3b7fe87d9fc7b
Bilal-Yasir34 <bilalyasir34@gmail.com>
2026-02-05 05:17:05 +0500

    Add physical admin redirect folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Change BrowserRouter to HashRouter
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './app';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
